521aaa61ecc55d42372c6ac752c6fb78fb3c297f
Manuel <Manuel>
2016-12-16 10:40:29 +0100

    seguimos trabajando en el 22

--- Lab22/Project1/Project1/SinglyLinkedList.h
+++ Lab22/Project1/Project1/SinglyLinkedList.h
@@ -10,11 +10,12 @@ template <class T>
 class SinglyLinkedList {
 public:
 	// Constructor sin parámetros
-	SinglyLinkedList() : _ini(nullptr) {}
+	SinglyLinkedList() : _ini(nullptr), tam(0){}
 
 	// Constructor copia
 	SinglyLinkedList(SinglyLinkedList const& other) {
 		_ini = copia(other._ini);
+		tam = other.size();
 	}
 
 	// destructor
@@ -28,6 +29,7 @@ public:
 		if (this != &other) {
 			libera(_ini);
 			_ini = copia(other._ini);
+			tam = other.size();
 		}
 		return *this;
 	}
@@ -46,6 +48,7 @@ public:
 	void push_front(T const& e) {
 		Nodo * nuevo = new Nodo(e, _ini);
 		_ini = nuevo;
+		tam++;
 	}
 
 	// Elimina el nodo del comienzo de la lista
@@ -54,6 +57,7 @@ public:
 		Nodo *aux = _ini;
 		_ini = _ini->_sig;
 		delete aux;
+		tam--;
 	}
 	//DuplicaLista
 	void duplicate_list () {
@@ -63,14 +67,63 @@ public:
 			aux2 = aux->_sig;
 			Nodo * nuevo = copia(aux2);
 			aux->sig = nuevo;
+			tam++;
 		}
 		aux = nullptr;
 		aux2 = nullptr;
 	}
 	void print_list() {
 		Nodo * aux = _ini;
+		while (aux != nullptr) {
+			cout << aux->_elem << " ";
+			aux = aux->_sig;
+		}
+	}
+	int size() {
+		return tam;
+	}
+	//Método para eliminar el mayor de la lista. Coste lineal
+	void eliminaMayor(){
+		Nodo * actual = _ini;
+		Nodo * previo = nullptr;
 
 
+		while (actual != nullptr) {
+			//Caso de que el mayor sea el primero.
+			if (actual == _ini) {
+				if (_ini->_sig->_elem < _ini->_elem) {
+					_ini = _ini->_sig;
+					actual = _ini;
+					previo = nullptr;
+					tam--;
+				}
+			}
+			else {
+				if (actual->_sig->_elem < actual->_elem) {
+					previo->_sig = actual->_sig;
+					actual = actual->_sig;
+					tam--;
+				}
+				else {
+					previo = actual;
+					actual = actual->_sig;
+				}
+			}
+		
+
+		}
+	}
+	//Metodo para invertir la lista
+	void invert() {
+		Nodo * aux = _ini->_sig;
+		Nodo * aux2 = aux->_sig;
+		_ini->_sig = nullptr;
+		while (aux != nullptr) {
+			aux->_sig = _ini;
+			_ini = aux;
+			aux = aux2;
+			if(aux!= nullptr)aux2 = aux->_sig;
+		}
 	}
 
 
@@ -83,6 +136,7 @@ private:
 	Clase nodo que almacena internamente el elemento (de tipo T),
 	y un puntero al nodo siguiente.
 	*/
+	int tam;
 	class Nodo {
 	public:
 		Nodo() : _sig(nullptr) {}
